e87212edede52a481183ca951bbce4392e562582
liushengjie <2501570676@qq.com>
2021-02-13 20:30:42 +0800

    完成页面布局优化

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,17 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    collapsed: false, // 为false时表示不贴近
   },
   mutations: {
+    collapsedChange(state) {
+      state.collapsed = !state.collapsed;
+    },
   },
   actions: {
+    collapsedChanged(context) {
+      context.commit('collapsedChange');
+    },
   },
   modules: {
   },
